8d612dbe87a18a3926891747656d7c469889b58a
sunwook1218 <sunwook1218@naver.com>
2021-03-22 01:52:24 +0900

    Add Controller

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@WebMvcTest
 class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
